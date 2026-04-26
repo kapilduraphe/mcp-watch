1e8e4a54f678fb54b34910319da3a6a04f86d5c2
Kapil Duraphe <kapil.vduraphe@gmail.com>
2026-04-25 19:36:46 -0700

    chore(deps): upgrade @typescript-eslint to v8, eslint to v9, fix postinstall

---
+++ eslint.config.cjs
@@ -0,0 +1,39 @@
+const tseslint = require('typescript-eslint');
+const eslint = require('@eslint/js');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-template': 'error',
+      'eqeqeq': ['error', 'always'],
+      'no-eval': 'error',
+      'no-implied-eval': 'error',
+      'no-new-func': 'error',
+      'no-script-url': 'error',
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'eslint.config.cjs'],
+  },
+);
